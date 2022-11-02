52efd8b2d318d2185afdda2785ec7cee6fb242d8
AlexaWhitDU <112434021+AlexaWhitDU@users.noreply.github.com>
2022-11-02 17:08:52 -0600

    updated dog class with stretch challenges

--- my_dogs.py
+++ my_dogs.py
@@ -2,6 +2,7 @@ import dog
 
 my_dog = dog.Dog("Bruno", "Super Shih Tzu")
 my_dog.bark()
+my_dog.sit()
 
 my_other_dog = dog.Dog("Ifrit", "Super Shiba Inu")
-print(my_other_dog.name)
+my_other_dog.roll_over()
