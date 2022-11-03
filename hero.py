0f56b5b6105aa692bbf407eaa22000b3f45c0e4f
AlexaWhitDU <112434021+AlexaWhitDU@users.noreply.github.com>
2022-11-02 22:33:06 -0600

    updated fight function - still debugging

--- hero.py
+++ hero.py
@@ -1,4 +1,6 @@
+from operator import truediv
 import random
+from timeit import repeat
 from ability import Ability
 from armor import Armor
 
@@ -12,7 +14,7 @@ class Hero:
         self.starting_health = starting_health
         self.current_health = starting_health
 
-    def fight(self, opponent):
+    def random_fight(self, opponent):
         winner = random.randint(0,1)        
         #Stretch Goal: fancy message
         if winner == 1:
@@ -54,11 +56,43 @@ class Hero:
         else:
             return True
 
+
     def fight(self, opponent): # Fight each hero until a victor emerges.
-        if self.abilities == 0:
+        fight = True 
+        hero2 = opponent
+        if hero1.abilities and hero2.abilities == 0:
             print("DRAW!")
+        # Start Fight!
         else:
-            while 
+            while fight is True:
+                if hero1.is_alive() is False:
+                    print (f"{hero2.name} won!")
+                    break
+                elif hero2.is_alive() is False:
+                    print (f"{hero1.name} won!")
+                    break
+                else:
+                    hero1.add_ability()
+                    hero2.add_ability()
+                    hero1.attack()
+                    hero2.defend()
+                    hero2.take_damage()
+                    hero1.take_damage()
+                    hero2.is_alive()
+                    if hero2.is_alive is False:
+                        print (f"{hero1.name} won!")
+                        break
+                    hero1.is_alive()
+                    if hero1.is_alive() is False:
+                        print (f"{hero2.name} won!")
+                        break
+                    hero2.attack()
+                    hero1.defend()
+                    hero1.take_damage()
+                    hero2.take_damage()
+                    repeat
+
+
   # 1) else, start the fighting loop until a hero has won
   # 2) the hero (self) and their opponent must attack each other and each must take damage from the other's attack
   # 3) After each attack, check if either the hero (self) or the opponent is alive
@@ -68,13 +102,18 @@ class Hero:
 
 
 
-
 if __name__ == "__main__":
-    hero = Hero("Grace Hopper", 200)
-    hero.take_damage(150)
-    print(hero.is_alive())
-    hero.take_damage(15000)
-    print(hero.is_alive())
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+    ability1 = Ability("Super Speed", 300)
+    ability2 = Ability("Super Eyes", 130)
+    ability3 = Ability("Wizard Wand", 80)
+    ability4 = Ability("Wizard Beard", 20)
+    hero1.add_ability(ability1)
+    hero1.add_ability(ability2)
+    hero2.add_ability(ability3)
+    hero2.add_ability(ability4)
+    hero1.fight(hero2)
     # hero1 = Hero("Wonder Woman")
     # hero2 = Hero("Dumbledore")
     # hero1.fight(hero2)
